88aff50b07d3b3d464a93245e07eb707bdcdc430
L BOWEN <ljbowen@l-bowens-mbp.attlocal.net>
2019-03-04 13:24:56 -0600

    added print not going good

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("This is not going GOOD")
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
