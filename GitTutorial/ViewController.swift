9677c564afec1655d575f4ad819a588ad425058d
L BOWEN <ljbowen@l-bowens-mbp.attlocal.net>
2019-03-04 13:10:31 -0600

    Reverted

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("hello CS193")
-        print("hello world!")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
